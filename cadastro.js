4e12c019d5c29b0a9d757ccfa56adf1e84558107
Gustavowhz <159265565+Gustavowhz@users.noreply.github.com>
2024-09-24 15:22:32 -0300

    cadastro.js

--- cadastro.js
+++ cadastro.js
@@ -1,38 +1,24 @@
-// Selecionando o formulário e a tabela
-//const productForm = document.getElementById('productForm');
-//const productList = document.getElementById('productList');
 const stockForm = document.getElementById('stockForm');
 const stockList = document.getElementById('stockList');
-
 const estoque = {
     'Cloro': 0,
     'Bomba': 0,
     'Peneira': 0,
     'Filtro': 0,
 };
-
 function atualizarEstoque(event) {
-    event.preventDefault(); // Evitar reload da página
-
-    // Capturando o produto e a quantidade
+    event.preventDefault(); 
     const produto = document.getElementById('produto').value;
     const quantidade = parseInt(document.getElementById('quantidade').value);
 
-    // Atualizando a quantidade do produto no objeto 'estoque'
     estoque[produto] += quantidade;
 
-    // Atualizando a exibição do estoque
     exibirEstoque();
     
-    // Limpando o campo de quantidade
     stockForm.reset();
 }
-
 function exibirEstoque() {
-    // Limpar a tabela antes de atualizar
     stockList.innerHTML = '';
-
-    // Adicionar cada produto e sua quantidade ao estoque
     for (let produto in estoque) {
         const newRow = document.createElement('tr');
         newRow.innerHTML = `
@@ -42,9 +28,5 @@ function exibirEstoque() {
         stockList.appendChild(newRow);
     }
 }
-
-// Adicionando evento para o formulário
 stockForm.addEventListener('submit', atualizarEstoque);
-
-// Exibir o estoque inicial (todos os produtos com quantidade 0)
 exibirEstoque();
